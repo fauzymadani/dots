a342eef198dbf7c5c449eae66c26f40e575af34b
fauzymadani <keperluansekolahfauzy@gmail.com>
2025-08-29 22:19:38 +0700

    idk what to write here

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -18,23 +18,24 @@ require("lazy").setup({
   {'nvim-tree/nvim-tree.lua'},
   { 'wakatime/vim-wakatime', lazy = false },
   {'lewis6991/gitsigns.nvim'},
+  
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' }
+  },
 })
 
 -- Set colorscheme
 local pywal16 = require('pywal16')
 pywal16.setup()
 
-require('mini.statusline').setup()
-
--- keymap for opening nvim-tree
-vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle NvimTree sidebar" })
-
--- telescope keymap
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+-- Statusline
+-- require('mini.statusline').setup()
+require("lualine").setup({
+  options = {
+    theme = "auto"
+  }
+})
 
 -- disable netrw 
 vim.g.loaded_netrw = 1
@@ -47,4 +48,40 @@ require("nvim-tree").setup({
   renderer = { group_empty = true },
   filters = { dotfiles = true },
 })
+
+-- KEYMAP SECTION
+-- set leader key to space
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- telescope keymap
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+local api = require("nvim-tree.api")
+
+-- <leader> + e => toggle/focus nvim-tree
+vim.keymap.set("n", "<leader>e", function()
+  if api.tree.is_visible() then
+    if vim.bo.filetype == "NvimTree" then
+      api.tree.toggle() 
+    else
+      api.tree.focus()  
+    end
+  else
+    api.tree.open()    
+  end
+end, { desc = "Toggle/focus NvimTree" })
+
+-- <leader> + q => close nvim tree if the cursor currently focused on nvim-tree
+vim.keymap.set("n", "<leader>q", function()
+  if vim.bo.filetype == "NvimTree" then
+    api.tree.close()
+  else
+    vim.cmd("q") -- fallback close buffer
+  end
+end, { desc = "Close NvimTree if focused" })
 
