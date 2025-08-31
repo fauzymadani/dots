915224f77d3f6cb5b6f6d4eec1bab67d2956b656
fauzymadani <keperluansekolahfauzy@gmail.com>
2025-08-31 10:51:01 +0700

    feat: add lsp support

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,33 +7,96 @@ vim.opt.rtp:prepend("~/.local/share/nvim/lazy/lazy.nvim")
 
 -- Setup lazy
 require("lazy").setup({
-  -- Base16 colorscheme
+  -- Colorschemes
   { "RRethy/base16-nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  -- Treesitter
   {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
+
+  -- Statusline
   { 'echasnovski/mini.statusline', version = false },
-  
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' }
+  },
+
+  -- Telescope
   { 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' } },
+
+  -- Utilities
   {"BurntSushi/ripgrep"},
   { 'uZer/pywal16.nvim', as = 'pywal16' },
   {'nvim-tree/nvim-tree.lua'},
   { 'wakatime/vim-wakatime', lazy = false },
   {'lewis6991/gitsigns.nvim'},
-  
+
+  -- Mason + LSP
   {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
+    "williamboman/mason.nvim",
+    opts = {}
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "neovim/nvim-lspconfig" }
+  },
+
+  -- Autocomplete
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip"
+    }
   },
 })
 
--- Set colorscheme
-local pywal16 = require('pywal16')
-pywal16.setup()
+-- Mason setup
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { "gopls" }, -- LSP Go
+})
+
+-- LSP setup
+local lspconfig = require("lspconfig")
+lspconfig.gopls.setup({}) -- pakai gopls langsung
+
+-- Autocomplete setup
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+  }, {
+    { name = "buffer" },
+  })
+})
+
+-- Colorscheme
+vim.cmd('colorscheme catppuccin-mocha')
 
 -- Statusline
--- require('mini.statusline').setup()
 require("lualine").setup({
   options = {
-    theme = "auto"
+    theme = "catppuccin"
   }
 })
 
@@ -50,20 +113,26 @@ require("nvim-tree").setup({
 })
 
 -- KEYMAP SECTION
--- set leader key to space
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- telescope keymap
+-- Telescope keymap
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
-local api = require("nvim-tree.api")
+-- LSP keymap
+vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+vim.keymap.set("n", "<leader>n", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
 
--- <leader> + e => toggle/focus nvim-tree
+-- Nvim-tree keymap
+local api = require("nvim-tree.api")
 vim.keymap.set("n", "<leader>e", function()
   if api.tree.is_visible() then
     if vim.bo.filetype == "NvimTree" then
@@ -76,12 +145,11 @@ vim.keymap.set("n", "<leader>e", function()
   end
 end, { desc = "Toggle/focus NvimTree" })
 
--- <leader> + q => close nvim tree if the cursor currently focused on nvim-tree
 vim.keymap.set("n", "<leader>q", function()
   if vim.bo.filetype == "NvimTree" then
     api.tree.close()
   else
-    vim.cmd("q") -- fallback close buffer
+    vim.cmd("q") 
   end
 end, { desc = "Close NvimTree if focused" })
 
--- vim/.vimrc
+++ vim/.vimrc
@@ -2,4 +2,14 @@ set number
 set relativenumber
 syntax on
 set nowrap 
+set termguicolors
 
+call plug#begin()
+
+" List your plugins here
+Plug 'tpope/vim-sensible'
+Plug 'catppuccin/vim', { 'as': 'catppuccin' }
+
+call plug#end()
+
+colorscheme catppuccin_mocha
